e5603b3835f4ed53a0b8066e501d71a53c509a50
Davit Julakidze <Davit.Julakidze@structuretone.com>
2018-06-07 16:46:38 -0400

    mmmm

--- src/opencv1.py
+++ src/opencv1.py
@@ -4,8 +4,16 @@ import matplotlib.pyplot as plt
 
 
 img = cv2.imread('car.jpg', cv2.IMREAD_GRAYSCALE)
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#cv2.imshow('image',img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
 
+#cv2.imshow('image', img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
 
+#plt.imshow(img, cmap='gray', interpolation='bicubic')
+#plt.plot([50,100],[80,100], 'c', linewidth=5)
+#plt.show()
+
+cv2.imwrite('cargray.png', img)
