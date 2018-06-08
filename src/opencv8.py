07f74014bdda9f315eb1689720b981d69498c167
Davit Julakidze <Davit.Julakidze@structuretone.com>
2018-06-08 15:50:55 -0400

    added blurs and starter morphological transofmartionas

--- src/opencv8.py
+++ src/opencv8.py
@@ -8,16 +8,27 @@ while True:
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
 
-    lower_pink = np.array([125,0,125])
+    lower_pink = np.array([130,0,130])
     upper_pink = np.array([255,255,255])
+
 
     mask = cv2.inRange(hsv, lower_pink, upper_pink)
     res = cv2.bitwise_and(frame, frame, mask= mask)
 
-    cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
 
+    kernel = np.ones((15,15), np.float32)/225
+    smoothed = cv2.filter2D(res, -1, kernel)
+
+    #different versions of blurs
+    blur = cv2.GaussianBlur(res, (15,15), 0)
+    median = cv2.medianBlur(res, 15)
+    bilateral = cv2.bilateralFilter(res, 15, 75, 75)
+
+
+
+    cv2.imshow('blur', blur)
+    cv2.imshow('median', median)
+    cv2.imshow('bilateral', bilateral)
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
--- src/opencv9.py
+++ src/opencv9.py
@@ -8,16 +8,20 @@ while True:
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
 
-    lower_pink = np.array([125,0,125])
+    lower_pink = np.array([130,0,130])
     upper_pink = np.array([255,255,255])
+
 
     mask = cv2.inRange(hsv, lower_pink, upper_pink)
     res = cv2.bitwise_and(frame, frame, mask= mask)
 
-    cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
+    # morphological transformations
 
+    
+
+
+    cv2.imshow('frame', frame)
+    cv2.imshow('res', res)
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
