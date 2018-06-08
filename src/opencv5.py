61865b9410aa2684e29a1e125182490226651fac
Davit Julakidze <Davit.Julakidze@structuretone.com>
2018-06-08 10:52:33 -0400

    complex operations in opencv

--- src/opencv5.py
+++ src/opencv5.py
@@ -3,14 +3,32 @@ import numpy as np
 
 
 img1 = cv2.imread('images/3D-Matplotlib.png')
-img2 = cv2.imread('images/mainsvmimage.png')
+img2 = cv2.imread('images/mainlogo.png')
 
-#add = img1 + img2 
-add = cv2.add(img1,img2)
+#adding images
+#weighted = cv2.addWeighted(img1, 0.6, img2, 0.4, 0)
+#cv2.imshow('weighted',weighted)
+
+rows,cols,channels = img2.shape
+roi = img1[0:rows, 0:cols]
 
 
+# masking the image to show it as black and white
+img2gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+ret, mask = cv2.threshold(img2gray, 220, 255, cv2.THRESH_BINARY_INV)
 
-cv2.imshow('add',add)
+# logical operations
+mask_inv = cv2.bitwise_not(mask)
+img1_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+img2_fg = cv2.bitwise_and(img2, img2, mask=mask)
+
+# adding main logo image to another image
+dst = cv2.add(img1_bg, img2_fg)
+img1[0:rows, 0:cols] = dst
+
+cv2.imshow('res', img1)
+
+
 cv2.waitKey(0)
 cv2.destroyAllWindows
 
